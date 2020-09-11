aac825f23547ad3ed5b3c8a7d4a3bb5e32e7d12f
jialuxu15 <33607618+jialuxu15@users.noreply.github.com>
2020-09-11 13:19:47 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,24 @@
 import copy
-
 class Solution:
-    def combinationSum(self, candidates: [int], target: int) -> [[int]]:
+    def combinationSum2(self, candidates: [int], target: int) -> [[int]]:
         
+        candidates.sort()
         def backtrack(num:list,target:int,choice:list,begin:int):
 
             if sum(num)==target:
+                if len(result)>0:
+                    if num == result[-1]:
+                        return
                 result.append(copy.deepcopy(num))
-                print(num,result) 
-                return 
+                 
             elif sum(num)>target:
                 return    
             for i in range(begin,len(choice)):
+                if i>0:
+                    if choice[i]==choice[i-1] and len(num)==0:
+                        continue
                 num.append(choice[i])
-                backtrack(num,target,choice,i)
+                backtrack(num,target,choice,i+1)
                 num.pop(-1)
                    
         result=[]
@@ -21,5 +26,4 @@ class Solution:
         return result
         
 a=Solution()
-print(a.combinationSum([2,3,7],7))
-
+print(a.combinationSum2([4,2,5,2,5,3,1,5,2,2],9))
